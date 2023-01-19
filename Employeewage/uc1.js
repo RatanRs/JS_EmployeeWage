78ef841d4a92b9d3aaebc0f2a9036f330c0cea46
9663461085 <ratansollapure16310@gmail.com>
2023-01-19 07:15:15 +0530

    Added:Calculated Employee daily wage

--- Employeewage/uc1.js
+++ Employeewage/uc1.js
@@ -1,9 +1,20 @@
-const Is_Absent=0
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let workingHours=0;
 
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==Is_Absent){
-    console.log("Employee is Absent");
-
-}else{
-    console.log("Employee is Present");
-}
+    let ramdomValue = Math.floor(Math.random() * 10) % 3;
+    switch (ramdomValue) {
+        case IS_FULL_TIME:
+            workingHours = FULL_TIME_HOURS;
+            break;
+        case IS_PART_TIME:
+            workingHours = PART_TIME_HOURS
+            break;
+        default:
+            workingHours = 0;
+    }
+let empWage=workingHours*WAGE_PER_HOUR;
+console.log("Employee Wage:"+empWage);
