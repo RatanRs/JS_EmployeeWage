0903d4937436034cfae08a2869418bea9fdf7ae5
9663461085 <ratansollapure16310@gmail.com>
2023-01-19 07:31:58 +0530

    Added:Calculated Employee wage for month

--- Employeewage/uc1.js
+++ Employeewage/uc1.js
@@ -3,23 +3,24 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-function getworkinghours(ramdomValue ) {
-      switch (ramdomValue) {
-        case IS_FULL_TIME:
-           return FULL_TIME_HOURS;
-
-        case IS_PART_TIME:
-            return PART_TIME_HOURS
-            
-        default:
-            return 0;
+const WORKING_DAYS_PER_MONTH = 20;
+    let workingHours = 0;
+    let day = 1;
+    while (day <= WORKING_DAYS_PER_MONTH) {
+        let ramdomValue = Math.floor(Math.random() * 10) % 3;
+        switch (ramdomValue) {
+            case IS_FULL_TIME:
+                workingHours += FULL_TIME_HOURS;
+                break;
+            case IS_PART_TIME:
+                workingHours += PART_TIME_HOURS
+                break;
+            default:
+                workingHours += 0;
+        }
+        day++;
     }
-}
-    let workingHours=0;
-    let ramdomValue = Math.floor(Math.random() * 10) % 3;
-    workingHours=getworkinghours(ramdomValue);
-    let empWage=workingHours*WAGE_PER_HOUR;
-    console.log("Employee wage per day : "+ empWage);
-    console.log("Employee working hrs : "+ workingHours);
+    console.log("Employee Working hours in month : " + workingHours);
+    console.log("Employee Total wage per month : " + workingHours * WAGE_PER_HOUR);
 
 
